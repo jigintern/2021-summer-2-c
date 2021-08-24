aea5805b3f3f64a5098de8e496d83bc68b99581a
Cidermonster <87560710+Cidermonster@users.noreply.github.com>
2021-08-24 18:13:16 +0900

    Add files via upload

---
+++ upload/js/upload.js
@@ -0,0 +1,16 @@
+function previewFile(file) {
+    const preview = document.getElementById('preview');
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const imageUrl = e.target.result;
+      const img = document.createElement("img");
+      img.src = imageUrl;
+      preview.appendChild(img);
+    }
+  
+    reader.readAsDataURL(file);
+  }
+  const file = document.getElementById("example").files[0];
+  const formData = new FormData();
+  formData.append("avatar", file);
+  fetch(送信先URL, { method: "POST", body: formData });
