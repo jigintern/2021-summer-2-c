9edba33f914697acfe64fa06d137aeb2cb683869
Rui0530 <87159451+Rui0530@users.noreply.github.com>
2021-08-26 10:02:13 +0900

    テスト
    テストです

---
+++ setumei.js
@@ -0,0 +1,9 @@
+function Saigai(){
+    for (let step = 0; step <6; step++){
+        if (a != 1){
+            let childnode = document.getElementById("pic"+step);
+            childnode.remove();
+        }
+    }
+}
+
