45f286831d05ba5089ae79b5fc445df48e02933a
Taisuke Fukuno <fukuno@jig.jp>
2021-08-28 10:45:40 +0900

    leaflet ver.

--- static/map.js
+++ static/map.js
@@ -1,125 +1,77 @@
+import { LeafletGSI } from "https://js.sabae.cc/LeafletGSI.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
+import { getDistance } from "https://js.sabae.cc/getDistance.js";
 
-window.init = async () => {
+onload = async () => {
+  const map = await LeafletGSI.initMap("divmap");
 
-    var kariX = 136.18598;
-    var kariY = 35.942432;
+  let kariX = 136.18598 + .0005;
+  let kariY = 35.942432;
 
-    console.log(kariY);
-    console.log(kariX);
+  const sabae = { lat: 35.943056, lng: 136.188889 };
+  const kari = { lat: kariY, lng: kariX };
+  map.setView([sabae.lat, sabae.lng], 15);
 
-    var sabae = { lat: 35.943056, lng: 136.188889 };
-    var kari = { lat: kariY, lng: kariX };
-    var map = new google.maps.Map(document.getElementById('map'),
-        {
-            zoom: 15,
-            center: sabae,
-        });
-    var marker = new google.maps.Marker({
-        position: sabae,
-        map: map,
-        icon: {
-            fillColor: "#0033FF",
-            fillOpacity: 0.8,
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 16,
-            strokeColor: "#0C0100",
-            strokeWeight: 1.0
-        },
-        label: {
-            text: '駅',
-            color: '#FFFFFF',
-            fontSize: '20px'
-        }
+  const marker = L.marker([sabae.lat, sabae.lng], {
+    icon: L.spriteIcon("green"),
+  }); // 駅
+  marker.addTo(map);
+
+  const karimarker = L.marker([kari.lat, kari.lng], {
+    icon: L.spriteIcon("red"),
+    draggable: true,
+  }); // 仮
+  karimarker.addTo(map);
+
+  karimarker.on("dragend", (e) => {
+    const ll = e.target.getLatLng();
+    latitude.value = ll.lat;
+    longitude.value = ll.lng;
+    kariY = ll.lat;
+    kariX = ll.lng;
+  });
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      const marker = L.marker([pos.lat, pos.lng], {
+        icon: L.spriteIcon("yellow"),
+      }); // 現在位置 google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+      marker.addTo(map);
+      map.panTo([pos.lat, pos.lng], 15);
     });
-    var karimarker = new google.maps.Marker({
-        position: kari,
-        map: map,
-        draggable: true,
-        icon: {
-            fillColor: "#FF2A00",
-            fillOpacity: 0.8,
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 16,
-            strokeColor: "#0C0100",
-            strokeWeight: 1.0
-        },
-        label: {
-            text: '仮',
-            color: '#FFFFFF',
-            fontSize: '20px'
-        }
+  } else {
+    alert("Err！");
+  }
+
+  const data = CSV.toJSON(await CSV.fetch("sabaesafe.csv"));
+  console.log(data);
+
+  for (const d of data) {
+    const X = Number(d["経度"]);
+    console.log("X", X);
+    const Y = Number(d["緯度"]);
+    console.log("Y", Y);
+    const opt = {};
+    const safemarker = L.marker([Y, X], opt);
+    safemarker.addTo(map);
+
+    safemarker.on("click", () => {
+      const dis = getDistance(kariY, kariX, Y, X); //km
+      let uploadFlg = false;
+      if (dis <= 0.01) {
+        uploadFlg = true;
+      }
+      console.log(uploadFlg);
+      console.log(X);
+      console.log(Y);
+      if (uploadFlg) {
+        location.href = "setumei/setumei.html?kouzui=" + d["洪水"] + "&doseki=" +
+          d["崖崩れ、土石流及び地滑り"] + "&jishin=" + d["地震"] + "&kaji=" + d["大規模な火事"] +
+          "&hanran=" + d["内水氾濫"];
+      }
     });
-    google.maps.event.addListener(karimarker, 'dragend', function (ev) {
-        document.getElementById('latitude').value = ev.latLng.lat();
-        document.getElementById('longitude').value = ev.latLng.lng();
-        kariY = ev.latLng.lat();
-        kariX = ev.latLng.lng();
-    });
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            var marker = new google.maps.Marker({
-                position: pos,
-                map: map,
-                icon: {
-                    fillColor: "#FF2A00",
-                    fillOpacity: 0.8,
-                    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-                    scale: 10,
-                    strokeColor: "#0C0100",
-                    strokeWeight: 1.0
-                },
-            });
-            map.setZoom(15);
-            map.setCenter(pos);
-        });
-    }
-    else {
-        alert("Err！");
-    }
-
-    const data = CSV.toJSON(await CSV.fetch("sabaesafe.csv"));
-    console.log(data);
-
-    for (const d of data) {
-        var X = Number(d["経度"]);
-        console.log("X", X);
-        var Y = Number(d["緯度"]);
-        console.log("Y", Y);
-        var sabaesafe = { lat: Y, lng: X };
-        var safemarker = new google.maps.Marker({
-            position: sabaesafe,
-            map: map,
-        });
-        var uploadFlg = false;
-        var dis;
-
-        google.maps.event.addListener(safemarker, 'click', ((Y2, X2) => {
-            return () => {
-                dis = distance(kariY, kariX, Y2, X2); //km
-                if (dis <= 0.01) {
-                    uploadFlg = true;
-                }
-                console.log(uploadFlg);
-                console.log(X);
-                console.log(Y);
-                if (uploadFlg) {
-                    location.href = "setumei/setumei.html?kouzui=" + d["洪水"] + "&doseki=" + d["崖崩れ、土石流及び地滑り"] + "&jishin=" + d["地震"] + "&kaji=" + d["大規模な火事"] + "&hanran=" + d["内水氾濫"];
-                }
-            };
-        })(Y, X));
-    }
-}
-
-const R = Math.PI / 180;
-function distance(lat1, lng1, lat2, lng2) {
-    lat1 *= R;
-    lng1 *= R;
-    lat2 *= R;
-    lng2 *= R;
-    return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
-}
+  }
+};
